fadc86d40b7fafea03f1837638c6edba0dbdf849
Manidweep <manidweep678@gmail.com>
2023-01-13 13:36:15 +0530

    lemonades: Get Riced

--- lineage_lemonades.mk
+++ lineage_lemonades.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonades device
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Ricedroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_lemonades
@@ -31,3 +31,37 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := OnePlus/OnePlus9R_IND/OnePlus9R:13/RKQ1.211119.001/R.c99469-1-1bad7:user/release-keys
+
+# riceDroid Flags
+
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+# Enable Google Recorder Functionality
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Opt out of google dialer support, compiler will build aosp dialer,
+# package type will change from PIXEL -> GMS
+TARGET_OPTOUT_GOOGLE_TELEPHONY := false
+
+# Flag for GrapheneOS Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+
+# Enable Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Enable Quick Tap Funtionality
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+# Enable Blur
+TARGET_ENABLE_BLUR := true
+
+# Set the correct chipset target to be shown in info
+RICE_CHIPSET := SD870
+
+# Maintainer Stuff
+RICE_MAINTAINER := Lord Itachi
+RICE_OFFICIAL := true
