dee8c4dbe589cf05b40713b19d5c1608d6b194b3
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-03-04 01:24:39 +0530

    Update and rename aosp_lemonades.mk to banana_lemonades.mk

--- banana_lemonades.mk
+++ banana_lemonades.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonades device
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
-# Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Ricedroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := aosp_lemonades
+PRODUCT_NAME := banana_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -26,8 +26,15 @@ PRODUCT_SYSTEM_DEVICE := OnePlus9R
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9R_IND-user 13 RKQ1.211119.001 R.c99469-1-1bad7 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := OnePlus/OnePlus9R_IND/OnePlus9R:13/RKQ1.211119.001/R.c99469-1-1bad7:user/release-keys
+    
+# stuffs
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+BANANA_MAINTAINER := Nacho
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
