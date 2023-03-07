947af76c708b872ca48de690c2cc743385c52ec9
sodalime0509 <76208711+sodalime0509@users.noreply.github.com>
2023-03-07 06:49:30 +0000

    Update and rename aosp_lemonades.mk to cherish_lemonades.mk

--- cherish_lemonades.mk
+++ cherish_lemonades.mk
@@ -12,9 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
 # Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-PRODUCT_NAME := aosp_lemonades
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+PRODUCT_NAME := cherish_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -24,6 +23,20 @@ PRODUCT_SYSTEM_NAME := OnePlus9R
 PRODUCT_SYSTEM_DEVICE := OnePlus9R
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Rom Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# FOD animations
+EXTRA_UDFPS_ANIMATIONS := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Nacho
+CHERISH_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus9R_IND-user 13 RKQ1.211119.001 R.d81137_1a386-b3 release-keys" \
